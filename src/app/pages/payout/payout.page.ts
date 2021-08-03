23e72fd82dbf074cabc55fbf351f0d3505dc2455
SalikBackEnd <salikahmed301@gmail.com>
2021-08-03 11:49:03 +0500

    Dividend and Bonus

--- src/app/pages/payout/payout.page.ts
+++ src/app/pages/payout/payout.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonInput, LoadingController, ModalController } from '@ionic/angular';
-import { HelperService } from 'src/app/services/helper.service';
+import { HelperService, Payout, Tables } from 'src/app/services/helper.service';
 import { LocalService } from 'src/app/services/local.service';
 import { ToastService } from 'src/app/services/toast.service';
 
@@ -15,9 +15,10 @@ export class PayoutPage implements OnInit {
     id:0,
     scriptid:"",
     type:0,
-    tax:0,
-    dividend:0,
-    bonus:0,
+    taxpercent:0,
+    percent:0,
+    taxamount:0,
+    Amount:0,
     date:Date.now()
   }
 
@@ -32,6 +33,7 @@ export class PayoutPage implements OnInit {
   public dividendAmount:number=0;
   public bonusqty:number=0;
   public tax:number=0;
+  public taxamount:number=0;
 
   constructor(
     private local: LocalService,
@@ -58,7 +60,6 @@ export class PayoutPage implements OnInit {
   
 
   DividendAmount(CurrentAmount,dividendpercentage){
-    
     return (dividendpercentage/100)*(CurrentAmount/10);
   }
   DividendTax(dividentAmount,taxpercentage){
@@ -80,6 +81,7 @@ export class PayoutPage implements OnInit {
     }else{
       this.tax=30;
     }
+    this.onFilerChange();
   }
 
   onDividendEnter() {
@@ -87,8 +89,8 @@ export class PayoutPage implements OnInit {
     if (this.Validation(this.textDividend)) {
       let currentamount = this.getAmountbyScript(this.selectedscriptid);
       this.dividendAmount = this.DividendAmount(currentamount, this.textDividend);
-      let taxamount = this.DividendTax(this.dividendAmount, this.tax);
-      this.dividendAmount = this.dividendAmount - taxamount;
+     this.taxamount = this.DividendTax(this.dividendAmount, this.tax);
+      this.dividendAmount = parseFloat((this.dividendAmount - this.taxamount).toFixed(3));
     }else{
       this.textDividend=0;
     }
@@ -103,14 +105,34 @@ export class PayoutPage implements OnInit {
     }
   }
 
-  onInput(value){
-    var reg = /^[0-9]\d*$/;
-    if(!reg.test(value)){
+  onInputDividend(value) {
+    if (this.selectedscriptid != (undefined || null)) {
+      var reg = /^[0-9]\d*$/;
+      if (!reg.test(value)) {
+        this.textDividend = null;
+        this.dividendField.value = '';
+      }
+    }else{
       this.textDividend=null;
-      this.dividendField.value='';
+      this.toast.show("Select a script!");
     }
   }
-
+  onInputBonus(value) {
+    if (this.selectedscriptid != (undefined || null)) {
+      var reg = /^[0-9]\d*$/;
+      if (!reg.test(value)) {
+        this.textBonus = null;
+        this.bonusField.value = '';
+      }
+    }else{
+      this.textBonus=null;
+      this.toast.show("Select a script!");
+    }
+  }
+  onFilerChange(){
+    this.onDividendEnter();
+    this.onBonusEnter();
+  }
   Validation(value){
    if(value<0){
       this.toast.show("Enter valid values!");
@@ -118,5 +140,46 @@ export class PayoutPage implements OnInit {
     }else{
       return true;
     }
+  }
+  pay(){
+      let list= this.populate();
+      this.local.SetData(Tables.Payout,list);
+      this.toast.show("Successfully payout!",500);
+  }
+  populate() {
+    let list: any = [];
+    let newid=0;
+    if (this.dividendChecked) {
+      newid=this.local.GenerateId(Tables.Payout);
+      this.Payout = {
+        id: newid,
+        scriptid: this.selectedscriptid,
+        type: Payout.Dividend,
+        taxpercent: this.tax,
+        taxamount: this.taxamount,
+        percent: this.textDividend,
+        Amount: this.dividendAmount,
+        date: Date.now()
+      }
+      list.push(this.Payout);
+    }
+    if (this.bonusChecked) {
+      let id=this.local.GenerateId(Tables.Payout);
+      if(newid==id){
+        id=id+1;
+      }
+      this.Payout = {
+        id: id,
+        scriptid: this.selectedscriptid,
+        type: Payout.Bonus,
+        taxpercent: this.tax,
+        taxamount: this.taxamount,
+        percent: this.textBonus,
+        Amount: this.bonusqty,
+        date: Date.now()
+      }
+      list.push(this.Payout);
+    }
+    return list;
   }
 }
